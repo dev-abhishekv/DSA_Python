86fcd74ff689c2098f875717cffd92dadc353e2d
dev.abhishek <dev.abhishekv@gmail.com>
2025-10-25 03:03:45 +0530

    Mid Point of LinkedList using slow and fast pointer approach

---
+++ linked_list_mid_point.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+def midPoint(head):
+    if head is None:
+        return head
+
+    slow = fast = head
+
+    while fast.next and fast.next.next:
+        slow = slow.next
+        fast = fast.next.next
+    
+    return slow
+
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, input().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+# Main
+t = int(input("Number of test cases: "))
+
+while t > 0 :
+    head = takeInput()
+    mid = midPoint(head)
+
+    if mid is not None :
+        print(mid.data)
+
+    t -= 1
