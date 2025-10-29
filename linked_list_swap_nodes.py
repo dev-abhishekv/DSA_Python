abb2aa135398fe5dec0db48c7d7800d18488ca98
dev.abhishek <dev.abhishekv@gmail.com>
2025-10-30 04:02:35 +0530

    Added Function to Swap Linked list via actual node swap

--- linked_list_swap_nodes.py
+++ linked_list_swap_nodes.py
@@ -33,12 +33,90 @@ def swapNodes(head, i, j):
 
     return head
 
+
+def swapActualNodes(head, i, j) :
+    """
+    Swap nodes at position i and j in a linked list by changing links.
+    """
+    # Count the no. of Node in the Linked List
+    index = -1
+
+    if head is None:
+        return head 
+    
+    if i == j:
+        return head
+    
+    FirstNode = SecondNode = None
+    FirstPrev = SecondPrev = None
+    
+    curr = head
+    
+    while curr:
+        index += 1
+        if index == i-1:
+            FirstPrev = curr
+            FirstNode = curr.next
+
+        if index == j-1:
+            SecondPrev = curr
+            SecondNode = curr.next
+        
+        if FirstPrev and SecondPrev:
+            break
+        
+        curr = curr.next
+    
+    if i == 0:
+        FirstNode = head
+        head = SecondNode
+        
+        SecondPrev.next = SecondNode.next
+        SecondNode.next = FirstNode.next
+        
+        FirstNode.next = SecondPrev.next
+        SecondPrev.next = FirstNode
+    elif SecondPrev == FirstNode:
+        SecondPrev.next =SecondNode.next
+        SecondNode.next = SecondPrev
+        
+        FirstPrev.next = SecondNode
+    else:
+        FirstPrev.next = FirstNode.next
+        SecondPrev.next = SecondNode.next
+
+        SecondNode.next = FirstPrev.next
+        FirstPrev.next = SecondNode
+
+        FirstNode.next = SecondPrev.next
+        SecondPrev.next = FirstNode
+    
+    return head
+
 # Basic structure of Linked List Operations
 # Definition for singly-linked list node
 class Node :
     def __init__(self, data) :
         self.data = data
         self.next = None
+
+
+def cloneLinkedList(head):
+    """
+    Return a deep copy of the linked list starting at head.
+    """
+    if head is None:
+        return None
+    new_head = Node(head.data)
+    new_tail = new_head
+    curr = head.next
+    while curr:
+        new_node = Node(curr.data)
+        new_tail.next = new_node
+        new_tail = new_node
+        curr = curr.next
+    return new_head
+
 
 # to take input linked list
 def takeInput() : 
@@ -77,6 +155,11 @@ def printLinkedList(head) :
 
 #main
 head = takeInput()
+clone_head = cloneLinkedList(head)
 m, n = map(int, input("Enter M and N: ").split())
+print("Swapping data of nodes:")
 head = swapNodes(head, m, n)
 printLinkedList(head)
+print("Swapping actual nodes:")
+clone_head = swapActualNodes(clone_head, m, n)
+printLinkedList(clone_head)
