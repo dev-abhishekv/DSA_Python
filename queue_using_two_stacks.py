38aef6f4857294a9e9259f870b28502fcb9b4368
dev.abhishek <dev.abhishekv@gmail.com>
2025-11-12 03:12:19 +0530

    Implementing queue using two stacks

--- queue_using_two_stacks.py
+++ queue_using_two_stacks.py
@@ -1,13 +1,13 @@
 class Queue:
     # Stacks to be used in the operations.
     def __init__(self):
-        self.stk1 = []
-        self.stk2 = []
+        self.__stk1 = []
+        self.__stk2 = []
 
     # Enqueues 'X' into the queue. Returns true after enqueuing.
     # Enqueue keeping O(1)
     def enqueue(self, X):
-        self.stk1.append(X)
+        self.__stk1.append(X)
         return True
 
     """
@@ -19,28 +19,28 @@ class Queue:
     """
     # Dequeue is O(n)
     def dequeue(self):
-        if len(self.stk1) == 0:
+        if len(self.__stk1) == 0:
             return -1
         
-        while len(self.stk1) > 1:
-            self.stk2.append(self.stk1.pop())
+        while len(self.__stk1) > 1:
+            self.__stk2.append(self.__stk1.pop())
         
-        ele = self.stk1.pop()
+        ele = self.__stk1.pop()
 
-        while self.stk2:
-            self.stk1.append(self.stk2.pop())
+        while self.__stk2:
+            self.__stk1.append(self.__stk2.pop())
         
         return ele
 
     def dequeueImproved(self):
-        if not self.stk1 and not self.stk2:
+        if not self.__stk1 and not self.__stk2:
             return -1
         
-        if not self.stk2:
-            while self.stk1:
-                self.stk2.append(self.stk1.pop())
+        if not self.__stk2:
+            while self.__stk1:
+                self.__stk2.append(self.__stk1.pop())
         
-        return self.stk2.pop()
+        return self.__stk2.pop()
 
 
 # Main
@@ -53,10 +53,15 @@ while t > 0:
 
     user_input = list(map(int, input("Operation and Value(for Enqueue): ").split()))
     option = user_input[0]
-    value = user_input[1] if len(user_input) == 2 else None
+    value = None
 
     if option == 1:
-        print("Enqueue: ", queue.enqueue(value))
+        if len(user_input) == 2:
+            value = user_input[1]
+            print("Enqueue: ", queue.enqueue(value))
+        else:
+            print("Enter the Value to be enqueued, Invalid Input")
+
     elif option == 2:
         print("Dequeue: ", queue.dequeue())
 
